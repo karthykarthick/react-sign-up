9f52e4e6e6e127317820853d7b7695b0bb432c45
karthick <karthylock@gmail.com>
2021-02-23 23:42:37 +0530

    added conditional statements to render different data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
